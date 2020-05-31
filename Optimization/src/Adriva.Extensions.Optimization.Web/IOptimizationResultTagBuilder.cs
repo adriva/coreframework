9a0654a11a6a670cf244f8eb76ad3a4513b81dd0
Murat Karatutlu <mnemonic@Ghost13.local>
2020-05-31 13:26:39 +0300

    documentation update

--- Optimization/src/Adriva.Extensions.Optimization.Web/IOptimizationResultTagBuilder.cs
+++ Optimization/src/Adriva.Extensions.Optimization.Web/IOptimizationResultTagBuilder.cs
@@ -12,9 +12,16 @@ namespace Adriva.Extensions.Optimization.Web
         /// <summary>
         /// Gets a value representing the supported extension by this tag builder.
         /// </summary>
-        /// <value></value>
+        /// <value>A string value that represents the extension of the assets, which this tag builder can process.</value>
         string Extension { get; }
 
+        /// <summary>
+        /// Generates an html tag and populates the attributes if needed for the given asset.
+        /// </summary>
+        /// <param name="options">A concrete implementation of ITagBuilderOptions that this tag builder may use.</param>
+        /// <param name="asset">The asset for which the html tag will be generated for.</param>
+        /// <param name="output">A concrete implementation of IHtmlContentBuilder that the output will be written to.</param>
+        /// <returns>Represents the asynchronous process operation.</returns>
         Task PopulateHtmlTagAsync(ITagBuilderOptions options, Asset asset, IHtmlContentBuilder output);
     }
 }
--- Optimization/src/Adriva.Extensions.Optimization.Web/OptimizationExtensions.cs
+++ Optimization/src/Adriva.Extensions.Optimization.Web/OptimizationExtensions.cs
@@ -6,9 +6,19 @@ using Microsoft.AspNetCore.Builder;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Provides extension methods to use Optimization services.
+    /// </summary>
     public static class OptimizationExtensions
     {
-        public static IOptimizationBuilder<WebOptimizationOptions> AddOptimization(this IServiceCollection services, Action<WebOptimizationOptions> configure)
+        /// <summary>
+        /// Adds the minimum required services to use the optimization system.
+        /// <remarks>This method does not register any transformation chains or tag builders.await You need to manually add them using the AddTransformationChain and AddTagBuilder extension methods.</remarks>
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add services to.</param>
+        /// <param name="configure">The WebOptimizationOptions configuration delegate.</param>
+        /// <returns>The Adriva.Extensions.Optimization.Abstractions.IOptimizationBuilder so that additional calls can be chained.</returns>
+        public static IOptimizationBuilder<WebOptimizationOptions> AddOptimizationCore(this IServiceCollection services, Action<WebOptimizationOptions> configure)
         {
             services.AddHttpContextAccessor();
             services.AddSingleton<OptimizationMiddleware>();
@@ -19,7 +29,19 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddOptimization<WebOptimizationOptions>(options =>
                 {
                     configure?.Invoke(options);
-                })
+                });
+        }
+
+        /// <summary>
+        /// Adds the default services to use the optimization system.
+        /// <remarks>This method registers default transformation chains and tag builders for javascript and css files.</remarks>
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add services to.</param>
+        /// <param name="configure">The WebOptimizationOptions configuration delegate.</param>
+        /// <returns>The Adriva.Extensions.Optimization.Abstractions.IOptimizationBuilder so that additional calls can be chained.</returns>
+        public static IOptimizationBuilder<WebOptimizationOptions> AddOptimization(this IServiceCollection services, Action<WebOptimizationOptions> configure)
+        {
+            return services.AddOptimizationCore(configure)
                 .AddTransformationChain("js", typeof(JavascriptBundleTransform), typeof(JavascriptMinificationTransform))
                 .AddTransformationChain("css", typeof(StylesheetBundleTransform), typeof(StylesheetMinificationTransform))
                 .AddTagBuilder<JavascriptResultTagBuilder>()
@@ -27,12 +49,24 @@ namespace Microsoft.Extensions.DependencyInjection
             ;
         }
 
-        public static IOptimizationBuilder<WebOptimizationOptions> AddTagBuilder<TBuilder>(this IOptimizationBuilder<WebOptimizationOptions> builder) where TBuilder : class, IOptimizationResultTagBuilder
+        /// <summary>
+        /// Adds a tag builder service to be used in optimization.
+        /// </summary>
+        /// <param name="builder">The Adriva.Extensions.Optimization.Abstractions.IOptimizationBuilder that the tag builder will be added to.</param>
+        /// <typeparam name="TBuilder">The type of the concrete class implementing IOptimizationResultTagBuilder interface.</typeparam>
+        /// <returns>The Adriva.Extensions.Optimization.Abstractions.IOptimizationBuilder so that additional calls can be chained.</returns>
+        public static IOptimizationBuilder<WebOptimizationOptions> AddTagBuilder<TBuilder>(this IOptimizationBuilder<WebOptimizationOptions> builder)
+                    where TBuilder : class, IOptimizationResultTagBuilder
         {
             builder.Services.AddSingleton<IOptimizationResultTagBuilder, TBuilder>();
             return builder;
         }
 
+        /// <summary>
+        /// Adds the Adriva.Extensions.Optimization.Web.OptimizationMiddleware to the specified Microsoft.AspNetCore.Builder.IApplicationBuilder, which enables asset optimization capabilities.
+        /// </summary>
+        /// <param name="app">The Microsoft.AspNetCore.Builder.IApplicationBuilder to add the middleware to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IApplicationBuilder UseOptimization(this IApplicationBuilder app)
         {
             app.UseMiddleware<OptimizationMiddleware>();
