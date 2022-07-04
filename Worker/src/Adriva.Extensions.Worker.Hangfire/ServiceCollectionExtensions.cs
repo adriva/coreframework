74ffcb6273bcddca96679cd6fc143dcdbfcf2023
mnemonic <info@adriva.com>
2022-07-04 16:22:06 +0300

    Hangfire ServiceCollectionExtensions refactored

--- Worker/src/Adriva.Extensions.Worker.Hangfire/ServiceCollectionExtensions.cs
+++ Worker/src/Adriva.Extensions.Worker.Hangfire/ServiceCollectionExtensions.cs
@@ -1,7 +1,10 @@
+using System;
 using Adriva.Extensions.Worker;
 using Adriva.Extensions.Worker.Hangfire;
 using Hangfire;
 using Hangfire.Common;
+using Hangfire.SqlServer;
+using Newtonsoft.Json;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -13,6 +16,42 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddSingleton<JobActivator, HangfireJobActivator>(serviceProvider => new HangfireJobActivator(serviceProvider, services))
                 .AddSingleton<IJobFilterProvider>(serviceProvider => JobFilterProviders.Providers)
                 .AddScheduledJobs<ScheduledJobsHost>();
+        }
+
+        public static IServiceCollection AddHangfire(this IServiceCollection services, HangfireOptions options, Action<JsonSerializerSettings> configureSerializer = null)
+        {
+            var globalConfiguration = GlobalConfiguration.Configuration;
+
+            SqlServerStorageOptions sqlServerStorageOptions = new SqlServerStorageOptions()
+            {
+                SchemaName = string.IsNullOrWhiteSpace(options.SchemaName) ? "rt" : options.SchemaName,
+                PrepareSchemaIfNecessary = true,
+                UseRecommendedIsolationLevel = true,
+                QueuePollInterval = TimeSpan.FromSeconds(60)
+            };
+
+            globalConfiguration.UseSqlServerStorage(options.ConnectionString, sqlServerStorageOptions);
+            globalConfiguration.UseColouredConsoleLogProvider();
+
+            options.AutomaticRetryCount = Math.Max(0, options.AutomaticRetryCount);
+
+            globalConfiguration.UseFilter<AutomaticRetryAttribute>(new AutomaticRetryAttribute()
+            {
+                Attempts = options.AutomaticRetryCount
+            });
+
+            if (null != configureSerializer)
+            {
+                JsonSerializerSettings defaultJsonSerializerSettings = new JsonSerializerSettings();
+                configureSerializer(defaultJsonSerializerSettings);
+                globalConfiguration.UseSerializerSettings(defaultJsonSerializerSettings);
+            }
+            else
+            {
+                globalConfiguration.UseRecommendedSerializerSettings();
+            }
+
+            return services;
         }
     }
 }
--- Worker/src/Adriva.Extensions.Worker.Hangfire/WorkerHostBuilderExtensions.cs
+++ Worker/src/Adriva.Extensions.Worker.Hangfire/WorkerHostBuilderExtensions.cs
@@ -1,8 +1,6 @@
 using System;
 using Adriva.Extensions.Worker;
 using Adriva.Extensions.Worker.Hangfire;
-using Hangfire;
-using Hangfire.SqlServer;
 using Newtonsoft.Json;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -13,36 +11,7 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             builder.HostBuilder.ConfigureServices((context, services) =>
             {
-                var globalConfiguration = GlobalConfiguration.Configuration;
-
-                SqlServerStorageOptions sqlServerStorageOptions = new SqlServerStorageOptions()
-                {
-                    SchemaName = string.IsNullOrWhiteSpace(options.SchemaName) ? "rt" : options.SchemaName,
-                    PrepareSchemaIfNecessary = true,
-                    UseRecommendedIsolationLevel = true,
-                    QueuePollInterval = TimeSpan.FromSeconds(60)
-                };
-
-                globalConfiguration.UseSqlServerStorage(options.ConnectionString, sqlServerStorageOptions);
-                globalConfiguration.UseColouredConsoleLogProvider();
-
-                options.AutomaticRetryCount = Math.Max(0, options.AutomaticRetryCount);
-
-                globalConfiguration.UseFilter<AutomaticRetryAttribute>(new AutomaticRetryAttribute()
-                {
-                    Attempts = options.AutomaticRetryCount
-                });
-
-                if (null != configureSerializer)
-                {
-                    JsonSerializerSettings defaultJsonSerializerSettings = new JsonSerializerSettings();
-                    configureSerializer(defaultJsonSerializerSettings);
-                    globalConfiguration.UseSerializerSettings(defaultJsonSerializerSettings);
-                }
-                else
-                {
-                    globalConfiguration.UseRecommendedSerializerSettings();
-                }
+                services.AddHangfire(options, configureSerializer);
             });
             return builder;
         }
